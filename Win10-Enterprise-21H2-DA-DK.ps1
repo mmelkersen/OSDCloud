304b0330417cef696d4c3f3476345e43e4dfe6f2
Mattias Melkersen <77377683+mmelkersen@users.noreply.github.com>
2021-09-21 21:16:42 +0200

    Update Win10-Enterprise-21H2-DA-DK.ps1

--- Win10-Enterprise-21H2-DA-DK.ps1
+++ Win10-Enterprise-21H2-DA-DK.ps1
@@ -16,7 +16,7 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSLanguage da-dk -OSBuild "20H2" -OSEdition Enterprise -ZTI
+Start-OSDCloud -OSLanguage da-dk -OSBuild "21H1" -OSEdition Enterprise -ZTI
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
